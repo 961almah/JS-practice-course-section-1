e61d3d3bb67b5511773246e28f60eccf94b1fa32
Ali Mahmoud <961alimahmoud@gmail.com>
2021-02-02 13:19:42 -0500

    did values and variables assignment

---
+++ script.js
@@ -0,0 +1,27 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Ali');
+console.log(23);
+
+let firstName = 'Ali'
+console.log(firstName);
+
+let ali_jiana = 'AM';
+let $function = 27;
+
+let person = 'ali'
+let PI = 3.1415;
+
+let myFirstJob = 'waiter';
+let myCurrentJob = 'programmer';
+
+// Values and Variables Assignment:
+
+let country = "USA"
+let continent = 'North America'
+let population = '300M people'
+
+console.log(country);
+console.log(continent);
+console.log(population);
