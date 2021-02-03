51b25f55ab6e80e61fff212f2480b456302ce979
Ali Mahmoud <961alimahmoud@gmail.com>
2021-02-03 10:19:26 -0500

    let const and var assignment

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
 
-console.log('Ali');
-console.log(23);
+// console.log('Ali');
+// console.log(23);
 
-let firstName = 'Ali'
-console.log(firstName);
+// let firstName = 'Ali'
+// console.log(firstName);
 
-let ali_jiana = 'AM';
-let $function = 27;
+// let ali_jiana = 'AM';
+// let $function = 27;
 
-let person = 'ali'
-let PI = 3.1415;
+// let person = 'ali'
+// let PI = 3.1415; 
 
-let myFirstJob = 'waiter';
-let myCurrentJob = 'programmer';
+// let myFirstJob = 'waiter';
+// let myCurrentJob = 'programmer';
 
-// Values and Variables Assignment:
+// // Values and Variables Assignment:
 
-let country = "USA"
-let continent = 'North America'
+const country = "USA"
+const continent = 'North America'
 let population = '300M people'
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Ali');
+
+// javaScriptIsFun = 'YES!';
+// console.log(typeof javaScriptIsFun);
+
+// Data Types Assignment
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+let age = 22;
+age = 23;
+
+const birthYear = 1998;
+
+// Let, Const, and Var Assignment
+
+language = 'English';
