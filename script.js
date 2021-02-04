07d876cff91086dfb162d614a4c87f3bb53d07a9
Ali Mahmoud <961alimahmoud@gmail.com>
2021-02-04 12:25:31 -0500

    code challenge #3

--- script.js
+++ script.js
@@ -133,22 +133,22 @@
 // TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
 // TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
 
-let markHeight = 1.69;
-let markWeight = 78;
+// let markHeight = 1.69;
+// let markWeight = 78;
 
-let johnHeight = 1.95;
-let johnWeight = 92;
+// let johnHeight = 1.95;
+// let johnWeight = 92;
 
-let markBMI = markWeight / markHeight ** 2
+// let markBMI = markWeight / markHeight ** 2
 
-let johnBMI = johnWeight / (johnHeight * johnHeight)
+// let johnBMI = johnWeight / (johnHeight * johnHeight)
 
-console.log(markBMI);
-console.log(johnBMI);
+// console.log(markBMI);
+// console.log(johnBMI);
 
-markHigherBMI = markBMI > johnBMI;
+// markHigherBMI = markBMI > johnBMI;
 
-console.log(markHigherBMI);
+// console.log(markHigherBMI);
 
 
 // ==============================================//
@@ -209,18 +209,110 @@ console.log(markHigherBMI);
 
 // =============================================== //
 
-const inputYear = '1991';
+// const inputYear = '1991';
 
-console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear), inputYear);
 
-console.log(Number(inputYear) + 18);
+// console.log(Number(inputYear) + 18);
 
 // =============================================== //
 
 // Type conversion and coercion assignment
 
-console.log('9' - '5', '19' - '13' + '17', '19' - '13' + 17, '123' < 57, 5 + 6 + '4' + 9 - 4 - 2);
+// console.log('9' - '5', '19' - '13' + '17', '19' - '13' + 17, '123' < 57, 5 + 6 + '4' + 9 - 4 - 2);
 
 // predictions: 4 617 23 false 1143
 
 // =============================================== //
+
+// Truthy falsy 
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Ali'));
+// console.log(Boolean({}));
+
+// let height;
+
+// if (height) {
+//     console.log(`yay`);
+// } else {
+//     console.log(`nay`);
+// }
+
+// =============================================== //
+
+// equality operators
+
+// const age = 18;
+
+// if (age === 18) console.log('You just became an adult');
+
+// const favorite = Number(prompt("what's your favorite number?"));
+
+// // without the Number you get a string and it doesn't work
+
+// if (favorite === 23) {
+//     console.log(`cool`);
+// } else if (favorite === 7) {
+//     console.log(`also cool`);
+// } else if (favorite === 9) {
+//     console.log(`cool as well`);
+// } else {
+//     console.log(`not cool`);
+// }
+
+// console.log(favorite);
+
+// =============================================== //
+
+// const hasDriversLicense = true;  //a
+// const hasGoodVision = true;  //b
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// const shouldDrive = hasDriversLicense && hasGoodVision;
+
+// if (shouldDrive) {
+//     console.log(`sarah can drive`);
+// } else {
+//     console.log(`someone else should drive`);
+// }
+
+// logical operators 
+
+//  ============================================= //
+
+////////////////////////////////////
+// Coding Challenge #3
+
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
+
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+GOOD LUCK 😀
+*/
+
+// const dolphinsScoreAverage = (96 + 108 + 89) / 3;
+
+// const KoalasScoreAverage = (88 + 91 + 110) / 3;
+
+// if (dolphinsScoreAverage > KoalasScoreAverage && dolphinsScoreAverage > 100) {
+//     console.log(`dolphins win`);
+// } else if (KoalasScoreAverage > dolphinsScoreAverage && KoalasScoreAverage > 100) {
+//     console.log(`koalas win`);
+// } else {
+//     console.log(`draw`);
+// }
+
